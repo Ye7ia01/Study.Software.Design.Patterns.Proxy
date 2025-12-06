b2f34eec278bd08dbb845f23fa817279937c7785
Ye7ia01 <whoami.iamcipher@gmail.com>
2025-12-06 14:53:24 +0200

    initial commit

---
+++ Software.Design.Patterns.Proxy/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Software.Design.Patterns.Proxy;
+
+SMSServiceProxy proxy = new SMSServiceProxy();
+// Will be sent
+Console.WriteLine(
+    proxy.SendSMS("1", "01003912122", "Hello"));
+// Will be sent
+Console.WriteLine(
+    proxy.SendSMS("1", "01003912122", "Hello"));
+// Will NOT be sent
+Console.WriteLine(
+    proxy.SendSMS("1", "01003912122", "Hello"));
